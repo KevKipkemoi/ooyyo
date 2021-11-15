522d3c97f5a649453ec4d67733f9e8f18ce94123
KevKipkemoi <KevKipkemoi@outlook.com>
2021-11-15 17:44:19 +0300

    constants file

--- ooyyo.py
+++ ooyyo.py
@@ -1,3 +1,4 @@
+import constants
 import xlsxwriter
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -20,20 +21,6 @@ class Scrapper:
         self.driver = webdriver.Chrome(service=_service, options=chrome_options)
         self.driver.get(_URL)
 
-        self.ANDROID_APP_MODAL = '//*[@id="o-modal-1636619015751"]/div'
-        self.ANDROID_APP_CANCEL_BTN = '//*[@id="o-modal-1636619015751"]/div/div/div[2]/button[1]'
-        self.SEARCH_BTN = '/html/body/section/div[1]/div/div/div[2]/div/div/div[5]/div[4]/a'
-        self.COUNTRY_DROPDOWN_INPUT = '/html/body/section/div[1]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/div/input'
-        self.COUNTRIES = '/html/body/section/div[1]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/div/ul'
-        self.RESULT_NUM = '/html/body/section/div[2]/h2'
-        self.CAR_DETAILS = '/html/body/section/div[2]/div/div[4]/a[1]/div[2]/div[2]/h2'
-        self.CAR_LOCATION = '/html/body/section/div[2]/div/div[4]/a[1]/div[2]/div[2]/div/div[1]/div[1]/span[1]/strong'
-        self.CAR_MILEAGE = '/html/body/section/div[2]/div/div[4]/a[1]/div[2]/div[2]/div/div[1]/div[3]/strong'
-        self.CAR_DETAILS = '/html/body/section/div[2]/div/div[4]/a[1]/div[2]/div[2]/div/div[1]/div[4]'
-        self.CAR_PRICE = '/html/body/section/div[2]/div/div[4]/a[1]/div[2]/div[2]/div/div[2]/div/span[2]'
-        self.CAR_CARD = '/html/body/section/div[2]/div/div[4]/a[1]'
-        self.CAR_COLUMN = '/html/body/section/div[2]'
-
         self.navigate_to_country(country = country)
 
     def navigate_to_country(self, country):
@@ -41,17 +28,18 @@ class Scrapper:
         if country == "Germany":
             pass
         else:
-            element = self.driver.find_element(By.XPATH, self.COUNTRY_DROPDOWN_INPUT)
+            element = self.driver.find_element(By.XPATH, constants.COUNTRY_DROPDOWN_INPUT)
             element.click()
-            li_elem = self.driver.find_element(By.XPATH, self.COUNTRIES).find_elements(By.TAG_NAME, 'li')
+            li_elem = self.driver.find_element(By.XPATH, constants.COUNTRIES).find_elements(By.TAG_NAME, 'li')
             for item in li_elem:
                 if item.text.split('\n')[0] == country:
                     item.click()
                     break
-        self.driver.find_element(By.XPATH, self.SEARCH_BTN).send_keys(Keys.RETURN)
+        self.driver.find_element(By.XPATH, constants.SEARCH_BTN).send_keys(Keys.RETURN)
+        self.get_car_details()
 
     def get_car_details(self):
-        soup = BeautifulSoup(self.driver.page_source)
+        soup = BeautifulSoup(self.driver.page_source, "html.parser")
         bs_info = soup.find_all("div", {"class": "info"})
         for info in bs_info:
             location = info.find("div", {"class": "location"}).get_text(strip=True)
