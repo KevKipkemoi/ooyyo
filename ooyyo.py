553c9576b836642bd6d3c37eeb9873cc7c1a536c
KevKipkemoi <KevKipkemoi@outlook.com>
2021-11-15 10:17:05 +0300

    get car details

--- ooyyo.py
+++ ooyyo.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -13,7 +14,7 @@ class Scrapper:
         # chrome_options.add_argument("--headless")
         _service = Service(ChromeDriverManager().install())
         self.total_results = 0
-        self.rest = []
+        self.car_details = []
         self.headers = []
         self.driver = webdriver.Chrome(service=_service, options=chrome_options)
         self.driver.get(_URL)
@@ -47,6 +48,17 @@ class Scrapper:
                     item.click()
                     break
         self.driver.find_element(By.XPATH, self.SEARCH_BTN).send_keys(Keys.RETURN)
+
+    def get_car_details(self):
+        soup = BeautifulSoup(self.driver.page_source)
+        bs_info = soup.find_all("div", {"class": "info"})
+        for info in bs_info:
+            location = info.find("div", {"class": "location"}).get_text(strip=True)
+            mileage = info.find("div", {"class": "mileage"}).get_text(strip=True)
+            description = info.find("div", {"class": "description"}).get_text(strip=True)
+            price_stat = info.find("div", {"class": "price-stat"}).get_text(strip=True)
+            price_info = info.find("div", {"class": "price-info"}).get_text(strip=True)
+            self.car_details.append([location, mileage, description, price_stat, price_info])
 
 
 if __name__ == '__main__':
