e33cc367d8e42b606cb7c74288068a8df65eea1a
KevKipkemoi <KevKipkemoi@outlook.com>
2021-11-16 13:02:09 +0300

    navigate to contact

--- ooyyo.py
+++ ooyyo.py
@@ -17,7 +17,8 @@ class Scrapper:
         _service = Service(ChromeDriverManager().install())
         self.total_results = 0
         self.car_details = []
-        self.headers = []
+        self.elements = []
+        self.main_window = None
         self.driver = webdriver.Chrome(service=_service, options=chrome_options)
         self.driver.get(_URL)
 
@@ -41,13 +42,14 @@ class Scrapper:
     def get_car_details(self):
         soup = BeautifulSoup(self.driver.page_source, "html.parser")
         bs_info = soup.find_all("div", {"class": "info"})
-        for info in bs_info:
+        for idx, info in enumerate(bs_info):
             location = info.find("div", {"class": "location"}).get_text(strip=True)
             mileage = info.find("div", {"class": "mileage"}).get_text(strip=True)
             description = info.find("div", {"class": "description"}).get_text(strip=True)
             price_stat = info.find("div", {"class": "price-stat"}).get_text(strip=True)
             price_info = info.find("div", {"class": "price-info"}).get_text(strip=True)
-            self.car_details.append([location, mileage, description, price_stat, price_info])
+            contact = self.navigate_to_contact(self.elements[idx], self.main_window)
+            self.car_details.append([location, mileage, description, price_stat, price_info, contact])
         self.write_data(self.car_details)
 
     def write_data(self, details):
@@ -56,6 +58,16 @@ class Scrapper:
         for row_num, it in enumerate(details):
             worksheet.write_row(row_num, 0, it)
         workbook.close()
+
+    def navigate_to_contact(self, element, main_window):
+        element.click()
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+        html_source_code = self.driver.execute_script("return document.body.innerHTML;")
+        html_soup = BeautifulSoup(html_source_code, 'html.parser')
+        cnt_btn = html_soup.find("a", {"id": "contactSeller"})
+        # self.driver.close()
+        self.driver.switch_to.window(main_window)
+        return cnt_btn['href']
 
 
 if __name__ == '__main__':
